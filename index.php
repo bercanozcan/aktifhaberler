f2c250f87cac02c2e13d77b15202cdabd96238cb
Bercan Özcan <beranozcan@gmail.com>
2020-09-10 18:39:21 +0300

    Update index.php

--- index.php
+++ index.php
@@ -13,7 +13,7 @@
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
                 <div class="col-md-6">
-                    <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
+                    <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-150 position-relative">
                         <div class="col p-4 d-flex flex-column position-static">
                             <strong class="d-inline-block mb-2 text-primary">World</strong>
                             <h3 class="mb-0 haber-baslik"><?php the_title(); ?></h3>
