262c11552ed01e020caa16eea4db26f8f7b696d4
Bercan Özcan <beranozcan@gmail.com>
2020-09-10 12:19:02 +0300

    Lang

--- index.php
+++ index.php
@@ -1,8 +1,8 @@
 <!doctype html>
-<html lang="en">
+<html lang="<?php language_attributes();?>">
 
 <head>
-    <meta charset="utf-8">
+    <meta charset="<?php bloginfo( 'charset' );?>">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
     <title>Aktif Haberler</title>
