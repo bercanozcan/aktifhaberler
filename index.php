7fb37ee62871e2a1a6cd6c31db3db434cb82ce59
Bercan Özcan <beranozcan@gmail.com>
2020-09-10 17:53:08 +0300

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@
     <div class="row mb-2">
         <?php if (have_posts()) : ?>
             <?php while (have_posts()) : the_post(); ?>
-                <div class="col-md-6">
+                <div class="col-md-4">
                     <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                         <div class="col p-4 d-flex flex-column position-static">
                             <strong class="d-inline-block mb-2 text-primary">World</strong>
