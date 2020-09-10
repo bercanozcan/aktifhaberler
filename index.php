8ad627f94e4084fa7f2280856c2dc9b27c0184ad
Bercan Özcan <beranozcan@gmail.com>
2020-09-10 18:50:34 +0300

    kategori

--- index.php
+++ index.php
@@ -1,10 +1,13 @@
 <?php get_header(); ?>
 
 <div class="jumbotron p-4 p-md-5 text-white rounded bg-dark">
-    <div class="col-md-6 px-0">
-        <h1 class="display-4 font-italic">Title of a longer featured blog post</h1>
-        <p class="lead my-3">Multiple lines of text that form the lede, informing new readers quickly and efficiently about what’s most interesting in this post’s contents.</p>
-        <p class="lead mb-0"><a href="#" class="text-white font-weight-bold">Continue reading...</a></p>
+    <div class="col-md-9 px-0">
+        <div class="manset-haberler">
+
+        </div>
+    </div>
+    <div class="col-md-3 px-0">
+
     </div>
 </div>
 
@@ -15,14 +18,12 @@
                 <div class="col-md-6">
                     <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-150 position-relative">
                         <div class="col p-4 d-flex flex-column position-static">
-                            <strong class="d-inline-block mb-2 text-primary"><?php
-
-                            $categories = get_the_category();
-                            if ( ! empty( $categories ) ) {
-                                echo '<a href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
-                            }
-
-                            ?></strong>
+                            <?php
+                                $categories = get_the_category();
+                                if(!empty($categories)){
+                                    echo '<a class="d-inline-block mb-2 text-primary" href="' . esc_url( get_category_link( $categories[0]->term_id ) ) . '">' . esc_html( $categories[0]->name ) . '</a>';
+                                }
+                            ?>
                             <h3 class="mb-0 haber-baslik"><?php the_title(); ?></h3>
                         </div>
                         <div class="col-auto d-none d-lg-block">
