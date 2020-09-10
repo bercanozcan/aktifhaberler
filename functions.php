34f47a4ad76f2f07a0592896d2d1bac3a9f51433
Bercan Özcan <beranozcan@gmail.com>
2020-09-10 13:22:36 +0300

    menu..

--- functions.php
+++ functions.php
@@ -3,9 +3,9 @@
     require_once get_template_directory() . '/nav.php';
 
     function ah_theme_support(){
-        register_nav_menus( [
-            'primary' => ___('Haber Kategori Menü')
-        ] );
+        register_nav_menus([
+            'primary' => __('Haber Kategori Menü')
+        ]);
     }
 
     add_action( 'after_theme_setup', 'ah_theme_support' );
