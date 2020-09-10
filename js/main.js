9c89068aebfdd351129da94a3b03f15a1d456c47
Bercan Özcan <beranozcan@gmail.com>
2020-09-10 19:18:10 +0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -22,26 +22,7 @@ $(() => {
             arrows: false,
             dots: false,
             focusOnSelect: false,
-            infinite: false,
-            responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 5,
-                    slidesToScroll: 5,
-                }
-            }, {
-                breakpoint: 640,
-                settings: {
-                    slidesToShow: 4,
-                    slidesToScroll: 4,
-                }
-            }, {
-                breakpoint: 420,
-                settings: {
-                    slidesToShow: 3,
-                    slidesToScroll: 3,
-                }
-            }]
+            infinite: false
         });
 
     $('.slider-single').on('afterChange', function (event, slick, currentSlide) {
