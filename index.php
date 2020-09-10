4a46c715f59ebac561c8a46f5a6410effc6f36af
Bercan Özcan <beranozcan@gmail.com>
2020-09-10 18:38:36 +0300

    post

--- index.php
+++ index.php
@@ -16,9 +16,7 @@
                     <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
                         <div class="col p-4 d-flex flex-column position-static">
                             <strong class="d-inline-block mb-2 text-primary">World</strong>
-                            <h3 class="mb-0"><?php the_title(); ?></h3>
-                            <div class="mb-1 text-muted"><?php the_time("j F Y H:i"); ?></div>
-                            <p class="card-text mb-auto"><?php echo 2; ?></p>
+                            <h3 class="mb-0 haber-baslik"><?php the_title(); ?></h3>
                         </div>
                         <div class="col-auto d-none d-lg-block">
                             <?php if (has_post_thumbnail()) : ?>
