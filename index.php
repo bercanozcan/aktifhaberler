5264106f3bd2b3b39ad6126e5aafde7e3dc03fd9
Bercan Özcan <beranozcan@gmail.com>
2020-09-10 16:16:38 +0300

    date

--- index.php
+++ index.php
@@ -16,7 +16,7 @@
             <div class="col p-4 d-flex flex-column position-static">
                 <strong class="d-inline-block mb-2 text-primary">World</strong>
                 <h3 class="mb-0"><?php the_title();?></h3>
-                <div class="mb-1 text-muted"><?php the_time();?></div>
+                <div class="mb-1 text-muted"><?php the_time("j F Y H:i");?></div>
                 <p class="card-text mb-auto"><?php echo 2;?></p>
             </div>
             <div class="col-auto d-none d-lg-block">
