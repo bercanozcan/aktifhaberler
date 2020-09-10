0d72d29b6296096955eec3c5e084f00ed6d062fd
Bercan Özcan <beranozcan@gmail.com>
2020-09-10 13:46:07 +0300

    haberler

--- index.php
+++ index.php
@@ -9,11 +9,13 @@
 </div>
 
 <div class="row mb-2">
+    <?php if(have_posts()) : ?>
+    <?php while(have_posts()) : ?>
     <div class="col-md-6">
         <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
             <div class="col p-4 d-flex flex-column position-static">
                 <strong class="d-inline-block mb-2 text-primary">World</strong>
-                <h3 class="mb-0">Featured post</h3>
+                <h3 class="mb-0"><?php the_title();?></h3>
                 <div class="mb-1 text-muted">Nov 12</div>
                 <p class="card-text mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
                 <a href="#" class="stretched-link">Continue reading</a>
@@ -26,23 +28,12 @@
             </div>
         </div>
     </div>
+    <?php endwhile; ?>
+    <?php else : ?>
     <div class="col-md-6">
-        <div class="row no-gutters border rounded overflow-hidden flex-md-row mb-4 shadow-sm h-md-250 position-relative">
-            <div class="col p-4 d-flex flex-column position-static">
-                <strong class="d-inline-block mb-2 text-success">Design</strong>
-                <h3 class="mb-0">Post title</h3>
-                <div class="mb-1 text-muted">Nov 11</div>
-                <p class="mb-auto">This is a wider card with supporting text below as a natural lead-in to additional content.</p>
-                <a href="#" class="stretched-link">Continue reading</a>
-            </div>
-            <div class="col-auto d-none d-lg-block">
-                <svg class="bd-placeholder-img" width="200" height="250" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="xMidYMid slice" focusable="false" role="img" aria-label="Placeholder: Thumbnail">
-                    <title>Placeholder</title>
-                    <rect width="100%" height="100%" fill="#55595c" /><text x="50%" y="50%" fill="#eceeef" dy=".3em">Thumbnail</text>
-                </svg>
-            </div>
-        </div>
+        <p>Mevcut haber yok.</p>
     </div>
+    <?php endif; ?>
 </div>
 </div>
 
